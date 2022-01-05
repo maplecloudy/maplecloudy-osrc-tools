cefa7d99cdc6b40fe153dad546dfb7ef50eae505
cpf <caopf@maplecloudy.com>
2022-01-05 20:04:34 +0800

    增加交换

--- maplecloudy-osrt-maven-plugin/src/main/java/com/maplecloudy/osrt/boot/maven/InstallOsrtAppMojo.java
+++ maplecloudy-osrt-maven-plugin/src/main/java/com/maplecloudy/osrt/boot/maven/InstallOsrtAppMojo.java
@@ -1,6 +1,41 @@
 package com.maplecloudy.osrt.boot.maven;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Maps;
+import com.maplecloudy.osrt.model.app.Config;
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.commons.httpclient.methods.multipart.ByteArrayPartSource;
+import org.apache.commons.httpclient.methods.multipart.FilePart;
+import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.artifact.ProjectArtifactMetadata;
+import org.apache.maven.shared.transfer.project.install.ProjectInstallerRequest;
+import org.springframework.util.ObjectUtils;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -21,101 +56,65 @@ import java.io.File;
  * under the License.
  */
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.logging.Logger;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.maplecloudy.osrt.model.app.Config;
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.multipart.ByteArrayPartSource;
-import org.apache.commons.httpclient.methods.multipart.FilePart;
-import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
-import org.apache.commons.httpclient.methods.multipart.Part;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.SystemUtils;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Execute;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.artifact.ProjectArtifactMetadata;
-import org.apache.maven.shared.transfer.project.install.ProjectInstallerRequest;
-
 /**
  * Installs the project's main artifact, and any other artifacts attached by
  * other plugins in the lifecycle, to osrt app center.
- * 
  */
 @Mojo(name = "install-osrt-app", defaultPhase = LifecyclePhase.INSTALL, requiresProject = true, threadSafe = true)
 @Execute(phase = LifecyclePhase.INSTALL)
 public class InstallOsrtAppMojo extends AbstractMojo {
-  
+
   /**
    * When building with multiple threads, reaching the last project doesn't have
    * to mean that all projects are ready to be installed
    */
   private static final AtomicInteger READYPROJECTSCOUTNER = new AtomicInteger();
-  
+
   private static final List<ProjectInstallerRequest> INSTALLREQUESTS = Collections
       .synchronizedList(new ArrayList<ProjectInstallerRequest>());
-  
+
   /**
    */
   @Parameter(defaultValue = "${project}", readonly = true, required = true)
   private MavenProject project;
-  
+
   @Parameter(defaultValue = "${reactorProjects}", required = true, readonly = true)
   private List<MavenProject> reactorProjects;
-  
+
   @Parameter(defaultValue = "${project.build.finalName}-osrc-app", readonly = true)
   private String finalName;
-  
+
   @Parameter
   private String classifier;
-  
+
   /**
    * Directory containing the generated archive.
-   * 
+   *
    * @since 1.0.0
    */
   @Parameter(defaultValue = "${project.build.directory}", required = true)
   private File outputDirectory;
-  
+
   /**
    * Whether every project should be installed during its own install-phase or
    * at the end of the multimodule build. If set to {@code true} and the build
    * fails, none of the reactor projects is installed.
    * <strong>(experimental)</strong>
-   * 
+   *
    * @since 2.5
    */
   @Parameter(defaultValue = "false", property = "installAtEnd")
   private boolean installAtEnd;
-  
+
   /**
    * Set this to <code>true</code> to bypass artifact installation. Use this for
    * artifacts that does not need to be installed in the local repository.
-   * 
+   *
    * @since 2.4
    */
   @Parameter(property = "install.osrt.skip", defaultValue = "false")
   private boolean skip;
-  
+
   public void execute() throws MojoExecutionException, MojoFailureException {
     if (skip) {
       getLog().info("Skipping install osrt app");
@@ -123,7 +122,7 @@ public class InstallOsrtAppMojo extends AbstractMojo {
       installProject();
     }
   }
-  
+
   private void installProject()
       throws MojoFailureException, MojoExecutionException {
     try {
@@ -131,29 +130,31 @@ public class InstallOsrtAppMojo extends AbstractMojo {
       getLog().info("install osrt Artifact:" + artifact);
       String packaging = project.getPackaging();
       File pomFile = project.getFile();
-      
+
       List<Artifact> attachedArtifacts = project.getAttachedArtifacts();
-      
+
       getLog().info("install osrt attachedArtifacts:" + attachedArtifacts);
       boolean isPomArtifact = "pom".equals(packaging);
-      
+
       ProjectArtifactMetadata metadata;
-      
+
       getLog().info("install osrt isPomArtifact:" + isPomArtifact);
       if (isPomArtifact)// app intall ingore pom Artifact
       {
         getLog().info("install osrt skip Pom Artifact!");
         return;
       }
-//        else {
-//        
-//        if (pomFile != null) {
-//          metadata = new ProjectArtifactMetadata(artifact, pomFile);
-//          artifact.addMetadata(metadata);
-//        }
-//      }
+      //        else {
+      //
+      //        if (pomFile != null) {
+      //          metadata = new ProjectArtifactMetadata(artifact, pomFile);
+      //          artifact.addMetadata(metadata);
+      //        }
+      //      }
       JarFile targerJar = null;
       PostMethod m = null;
+      GetMethod mg = null;
+      ObjectMapper om = new ObjectMapper();
       try {
         File target = getTargetFile(this.finalName, this.classifier,
             this.outputDirectory);
@@ -164,61 +165,191 @@ public class InstallOsrtAppMojo extends AbstractMojo {
         String osrtAppSite = System.getenv("OSRT_APP_SITE");
         String osrtAppToken = System.getenv("OSRT_APP_TOKEN");
         File osrcFile = new File(SystemUtils.getUserHome(), ".osrc");
-        ObjectMapper om = new ObjectMapper();
-        Config config= om.readValue(osrcFile, Config.class);
+        boolean exists = osrcFile.exists();
+        Config config = null;
+        if (exists) {
+          config = om.readValue(osrcFile, Config.class);
+        }
         if (StringUtils.isBlank(osrtAppSite)) {
-          osrtAppSite = config.getRemote()+"/api/apps/install-app-file";
+          if (!ObjectUtils.isEmpty(config) && !ObjectUtils
+              .isEmpty(config.getRemote())) {
+            osrtAppSite = config.getRemote();
+          } else {
+            config = new Config();
+            config.setRemote("https://www.osrc.com");
+            osrtAppSite = config.getRemote();
+            //getLog().error("wrong configuration for remote!");
+            //return;
+          }
         }
-        m = new PostMethod(osrtAppSite);
-//        Header header = new Header("", "multipart/form-data");
-//      Header header = new Header("Content-type","multipart/form-data");
-//      FilePart filePath = new FilePart(target.getName(),target);
         if (StringUtils.isBlank(osrtAppToken)) {
-          osrtAppToken = config.getAccessToken();
+          if (!ObjectUtils.isEmpty(config) && !ObjectUtils
+              .isEmpty(config.getAccessToken())) {
+            osrtAppToken = config.getAccessToken();
+          }
         }
-        getLog().info(osrtAppSite);
-        getLog().info(osrtAppToken);
-        Header header = new Header("Authorization", "Bearer "+osrtAppToken);
+        Header header = new Header("Authorization", "Bearer " + osrtAppToken);
+        if (ObjectUtils.isEmpty(osrtAppToken)) {
+          Scanner sc = new Scanner(System.in);
+          while (true) {
+            getLog().info("try to login (y/n)? ");
+            String yn = sc.nextLine();
+            if ("y".equalsIgnoreCase(yn) || "yes".equalsIgnoreCase(yn)) {
+              getLog().info("please input the username: ");
+              String username = sc.nextLine();
+              getLog().info("please input the password: ");
+              String password = sc.nextLine();
+              m = new PostMethod(osrtAppSite + "/api/accounts/signin");
+              Map<String,String> map = Maps.newHashMap();
+              map.put("username", username);
+              map.put("password", password);
+              String userInfoStr = om.writeValueAsString(map);
+              RequestEntity entity = new StringRequestEntity(userInfoStr,
+                  "application/json", "UTF-8");
+              m.setRequestEntity(entity);
+              int code = hc.executeMethod(m);
+              if (code == 200) {
+                getLog().info("login successfully!");
+                //写入.osrc文件
+                Map tokenMap = om.readValue(m.getResponseBody(), Map.class);
+                String token = tokenMap.get("accessToken").toString();
+                config.setAccessToken(token);
+                config.setUsername(username);
+                config.setTokenType("Bearer");
+                config.setRemote(osrtAppSite);
+                om.writeValue(osrcFile, config);
+                osrtAppToken = token;
+                header = new Header("Authorization", "Bearer " + osrtAppToken);
+                break;
+              } else {
+                getLog().error(
+                    "install osrt file:" + target + " failed with code: " + code
+                        + ",error message:" + m.getResponseBodyAsString());
+              }
+            } else if ("n".equalsIgnoreCase(yn) || "no".equalsIgnoreCase(yn)) {
+              getLog().info("Skipping install osrt app");
+              return;
+            } else {
+              continue;
+            }
+          }
+        } else {
+          //verify token
+          mg = new GetMethod(osrtAppSite + "/api/user");
+          mg.addRequestHeader(header);
+          int mgRespStatus = hc.executeMethod(mg);
+          if (mgRespStatus == 200) {
+            Map map = om.readValue(mg.getResponseBody(), Map.class);
+            String username = map.get("username").toString();
+            getLog().info("Install app will be executed by " + username + "!");
+          } else {
+            getLog().error("Failed to obtain user information! ");
+            getLog().error(
+                "failed with code: " + mgRespStatus + ",error message:" + mg
+                    .getResponseBodyAsString());
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+              getLog().info("try to login (y/n)? ");
+              String yn = sc.nextLine();
+              if ("y".equalsIgnoreCase(yn) || "yes".equalsIgnoreCase(yn)) {
+                getLog().info("please input the username: ");
+                String username = sc.nextLine();
+                getLog().info("please input the password: ");
+                String password = sc.nextLine();
+                m = new PostMethod(osrtAppSite + "/api/accounts/signin");
+                Map<String,String> map = Maps.newHashMap();
+                map.put("username", username);
+                map.put("password", password);
+                String userInfoStr = om.writeValueAsString(map);
+                RequestEntity entity = new StringRequestEntity(userInfoStr,
+                    "application/json", "UTF-8");
+                m.setRequestEntity(entity);
+                int code = hc.executeMethod(m);
+                if (code == 200) {
+                  getLog().info("login successfully!");
+                  //写入.osrc文件
+                  Map tokenMap = om.readValue(m.getResponseBody(), Map.class);
+                  String token = tokenMap.get("accessToken").toString();
+                  config.setAccessToken(token);
+                  config.setUsername(username);
+                  config.setTokenType("Bearer");
+                  config.setRemote(osrtAppSite);
+                  om.writeValue(osrcFile, config);
+                  osrtAppToken = token;
+                  header = new Header("Authorization",
+                      "Bearer " + osrtAppToken);
+                  break;
+                } else {
+                  getLog().error(
+                      "install osrt file:" + target + " failed with code: "
+                          + code + ",error message:" + m
+                          .getResponseBodyAsString());
+                }
+              } else if ("n".equalsIgnoreCase(yn) || "no"
+                  .equalsIgnoreCase(yn)) {
+                getLog().info("Skipping install osrt app");
+                return;
+              } else {
+                continue;
+              }
+            }
+          }
+        }
+
+        m = new PostMethod(osrtAppSite + "/api/apps/install-app-file");
+        //        Header header = new Header("", "multipart/form-data");
+        //      Header header = new Header("Content-type","multipart/form-data");
+        //      FilePart filePath = new FilePart(target.getName(),target);
+        //getLog().info(osrtAppSite);
+        //getLog().info(osrtAppToken);
         m.addRequestHeader(header);
         targerJar = new JarFile(target);
-        
+
         JarEntry indexEntry = targerJar.getJarEntry("index.yml");
         if (indexEntry == null) {
-          getLog().info("install osrt skip package not have [index.yml]!");
+          getLog().error("install osrt skip package not have [index.yml]!");
           return;
         }
-        byte[] index= sun.misc.IOUtils.readFully(targerJar.getInputStream(indexEntry) ,(int) indexEntry.getSize(), true);
-        ByteArrayPartSource bps = new ByteArrayPartSource("index.yml",index);
+        byte[] index = sun.misc.IOUtils
+            .readFully(targerJar.getInputStream(indexEntry),
+                (int) indexEntry.getSize(), true);
+        ByteArrayPartSource bps = new ByteArrayPartSource("index.yml", index);
         Part[] parts = {
-            new FilePart("index", bps,FilePart.DEFAULT_CONTENT_TYPE,StandardCharsets.UTF_8.name()),
+            new FilePart("index", bps, FilePart.DEFAULT_CONTENT_TYPE,
+                StandardCharsets.UTF_8.name()),
             new FilePart("appFile", target)};
-        
+
         MultipartRequestEntity mre = new MultipartRequestEntity(parts,
             m.getParams());
-//      FileRequestEntity fileRequestEntity = new FileRequestEntity(target,"multipart/form-data");
+        //      FileRequestEntity fileRequestEntity = new FileRequestEntity(target,"multipart/form-data");
         m.setRequestEntity(mre);
-        
+
+        getLog().info("begin to deploy app...");
         int respStatus = hc.executeMethod(m);
         if (respStatus == 200) {
           getLog().info("install osrt file:" + target + " sucesss!");
+          getLog().info("App info: " + m.getResponseBodyAsString());
         } else {
-          getLog().info("install osrt file:" + target + " failed with code: "
-              + respStatus + ",error message:" + m.getResponseBodyAsString());
-          
+          getLog().error(
+              "install osrt file:" + target + " failed with code: " + respStatus
+                  + ",error message:" + m.getResponseBodyAsString());
         }
       } finally {
         IOUtils.closeQuietly(targerJar);
         if (m != null) {
           m.releaseConnection();
         }
+        if (mg != null) {
+          mg.releaseConnection();
+        }
       }
-      
+
     } catch (IOException e) {
       throw new MojoFailureException("IOException", e);
     }
-    
+
   }
-  
+
   protected File getTargetFile(String finalName, String classifier,
       File targetDirectory) {
     String classifierSuffix = (classifier != null) ? classifier.trim() : "";
@@ -228,11 +359,32 @@ public class InstallOsrtAppMojo extends AbstractMojo {
     if (!targetDirectory.exists()) {
       targetDirectory.mkdirs();
     }
-    return new File(targetDirectory, finalName + classifierSuffix + "."
-        + this.project.getArtifact().getArtifactHandler().getExtension());
+    return new File(targetDirectory,
+        finalName + classifierSuffix + "." + this.project.getArtifact()
+            .getArtifactHandler().getExtension());
   }
-  
+
   public void setSkip(boolean skip) {
     this.skip = skip;
+  }
+
+  public static void main(String[] args) throws IOException {
+    HttpClient hc = new HttpClient();
+    PostMethod m = null;
+    String url = "https://www.osrc.com/api/accounts/signin";
+    m = new PostMethod(url);
+
+    String body = "{\"username\":\"ccc\",\"password\":\"000000\"}";
+    RequestEntity entity = new StringRequestEntity(body, "application/json",
+        "UTF-8");
+    m.setRequestEntity(entity);
+
+    int code = hc.executeMethod(m);
+    System.out.println(code);
+    ObjectMapper om = new ObjectMapper();
+
+    Map map = om.readValue(m.getResponseBody(), Map.class);
+    System.out.println(map);
+
   }
 }
