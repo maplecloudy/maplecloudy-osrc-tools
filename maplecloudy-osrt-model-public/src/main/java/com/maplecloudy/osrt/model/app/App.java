513fc582a3ae68a717bc01607c2ff5bdf47857cd
cpf <caopf@maplecloudy.com>
2022-07-22 01:23:23 +0800

    temp

--- maplecloudy-osrt-model-public/src/main/java/com/maplecloudy/osrt/model/app/App.java
+++ maplecloudy-osrt-model-public/src/main/java/com/maplecloudy/osrt/model/app/App.java
@@ -1,9 +1,12 @@
 package com.maplecloudy.osrt.model.app;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
-
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class App extends AbstractApp{
   @Schema(description = "此应用依赖的tools应用")
   private List<Tools> tools;
